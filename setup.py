d355f9a4cc036db63ca3bd3748a36dc62e74579a
汪威 <wangwei@nullmax.ai>
2023-07-04 18:09:05 +0800

    rename package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="general_utils",
+    name="gutils",
     version="1.0",
     author="weiwang",
     author_email="weiwang201806@gmail.com",
