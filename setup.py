1bf67c8a7a0386de76ca50b9eb550af245bc6c01
wangwei <wangwei@nullmax.ai>
2024-09-10 14:31:16 +0800

    opencv depend version 4.2.0.32

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=find_packages(exclude=["test"]),
     install_requires=[
         'numpy>=1.19.0',
-        'opencv-python>=4.2.0.34',
+        'opencv-python>=4.2.0.32',
         'Pillow>=8.0.0',
         'torch>=1.6.0',
         'tqdm>=4.11.0',
