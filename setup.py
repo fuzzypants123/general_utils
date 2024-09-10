f208ef5679148f8ee8de4412aec93ef9bd6c1bef
wangwei <wangwei@nullmax.ai>
2024-09-10 14:30:07 +0800

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gutils",
-    version="0.0.1",
+    version="0.0.2",
     author="weiwang",
     author_email="weiwang201806@gmail.com",
     description="my python general utils",
